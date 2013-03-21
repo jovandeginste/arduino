06d3facbb9a8c6e2c97b2e5c29e7f13939c072a2
Jo Vandeginste <jo.vandeginste@gmail.com>
2013-03-21 05:16:34 +0100

    improved

--- w1sender/src/w1sender.ino
+++ w1sender/src/w1sender.ino
@@ -16,8 +16,15 @@
 #include <Ports.h>
 #include <avr/sleep.h>
 
-// if B_MODEL defined, temp reading will use 18B20 model scheme, otherwise 18S20 is assumed
-#define B_MODEL
+#define LED_PIN     9   // activity LED, comment out to disable
+//#define DEBUG
+
+static void activityLed (byte on) {
+#ifdef LED_PIN
+	pinMode(LED_PIN, OUTPUT);
+	digitalWrite(LED_PIN, !on);
+#endif
+}
 
 OneWire  ds(4);
 
@@ -37,6 +44,7 @@ void setup() {
 
 	rf12_initialize(7, RF12_868MHZ, 33);
 	Serial.println("Temp transmiter startup");
+	activityLed(0);
 }
 
 bool Next1820(byte *addr) {
@@ -68,9 +76,7 @@ byte *Get1820Tmp(byte *addr) {
 	ds.select(addr);
 	ds.write(0x44,1);         // start conversion, with parasite power on at the end
 
-	//delay(1000);     // maybe 750ms is enough, maybe not
-	deep_sleep(1000);
-	// we might do a ds.depower() here, but the reset will take care of it.
+	deep_sleep(1000);     // maybe 750ms is enough, maybe not
 
 	present = ds.reset();
 	ds.select(addr);
@@ -80,6 +86,7 @@ byte *Get1820Tmp(byte *addr) {
 		OneWData[i] = ds.read();
 	}
 
+#ifdef DEBUG
 	// 18B20 default to 12 bits resolution at power up
 	LowByte = OneWData[0];
 	HighByte = OneWData[1];
@@ -95,50 +102,54 @@ byte *Get1820Tmp(byte *addr) {
 
 	if (ds.crc8(OneWData, 8) != OneWData[8]) Serial.println("BAD CRC !");
 	TempAsStringLen = sprintf(TempAsString, "%c%d.%d",SignBit != 0 ? '-' : '+', Whole, Fract);
+#endif
 	return OneWData;
 }
 
 void deep_sleep(int milli) {
-	delay(milli);
-	//	long limit = millis() + milli;
-	//	while(millis() != limit) {
-	//		set_sleep_mode(SLEEP_MODE_IDLE);
-	//		sleep_mode();
-	//	}
+	long limit = millis() + milli;
+	while(millis() <= limit) {
+		set_sleep_mode(SLEEP_MODE_IDLE);
+		sleep_mode();
+	}
 }
 
+void rf12_send(byte header, const void* data, byte length) {
+#ifdef LED_PIN
+	activityLed(1);
+#endif
+	rf12_recvDone();
+	if (rf12_canSend()) {
+		rf12_sendStart(header, data, length);
+		rf12_sendWait(2);
+		deep_sleep(10);
+	}
+#ifdef LED_PIN
+	activityLed(0);
+#endif
+}
 void loop() {
 	// Note that even if you only want to send out packets, you still have to call rf12 recvDone periodically, because
 	// it keeps the RF12 logic going. If you don't, rf12_canSend() will never return true.
 	count = 0;
 	while (Next1820(addr)) {
+		count += 1;
 		temp = Get1820Tmp(addr);
 		LowByte = temp[0];
 		HighByte = temp[1];
 		Whole = (HighByte << 4) | (LowByte >> 4);
 		Fract = ((LowByte & 0b1111) * 10) / 16;
 
-		Serial.println("New Sending ");
-
-		count += 1;
-		delay(100);
-		rf12_recvDone();
-		if (rf12_canSend()) {
-			TempBinFormat[0] = count;
-			memcpy(&TempBinFormat[1], addr, 8); 
-
-			TempBinFormat[9] = Whole;
-			TempBinFormat[10] = Fract;
-			rf12_sendStart(0, TempBinFormat, sizeof(TempBinFormat));
-		}
-		Serial.println("Sending done");
-	}
-	delay(100);
-	rf12_recvDone();
-	if (rf12_canSend()) {
 		TempBinFormat[0] = count;
-		rf12_sendStart(0, TempBinFormat, 1);
+		memcpy(&TempBinFormat[1], addr, 8); 
+
+		TempBinFormat[9] = Whole;
+		TempBinFormat[10] = Fract;
+		rf12_send(0, TempBinFormat, 15);
 	}
+	TempBinFormat[0] = 0;
+	TempBinFormat[1] = count;
+	rf12_send(0, TempBinFormat, 2);
 	deep_sleep(3000);
 }
 
